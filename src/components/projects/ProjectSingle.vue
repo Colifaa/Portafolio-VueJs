24d1f9c49a25d110d6227dc1fe9afb4a362cb5bb
unknown <Jorgemathez14@gmail.com>
2024-11-09 16:50:20 -0300

    modificaciones

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -5,9 +5,8 @@ export default {
 </script>
 
 <template>
-	<router-link
-		to="/projects/single-project"
-		class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
+	<div
+		class="rounded-xl shadow-lg hover:shadow-xl mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark"
 		aria-label="Single Project"
 	>
 		<div>
@@ -28,7 +27,7 @@ export default {
 				>{{ project.category }}</span
 			>
 		</div>
-	</router-link>
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
