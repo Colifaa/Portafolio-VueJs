0e1ced99506ef210df59c222d586362ca69b04a5
unknown <Jorgemathez14@gmail.com>
2024-11-10 17:18:28 -0300

    Definitivo

--- src/components/HireMeModal.vue
+++ src/components/HireMeModal.vue
@@ -99,7 +99,7 @@ export default {
 
 									<div class="mt-7 pb-4 sm:pb-1">
 										<Button
-											title="Send Request"
+											title="Enviar"
 											class="px-4 sm:px-6 py-2 sm:py-2.5 text-white bg-indigo-500 hover:bg-indigo-600 rounded-md focus:ring-1 focus:ring-indigo-900 duration-500"
 											type="submit"
 											aria-label="Submit Request"
@@ -111,7 +111,7 @@ export default {
 								class="modal-footer mt-2 sm:mt-0 py-5 px-8 border0-t text-right"
 							>
 								<Button
-									title="Close"
+									title="Cerrar"
 									class="px-4 sm:px-6 py-2 bg-gray-600 text-primary-light hover:bg-ternary-dark dark:bg-gray-200 dark:text-secondary-dark dark:hover:bg-primary-light rounded-md focus:ring-1 focus:ring-indigo-900 duration-500"
 									@click="showModal()"
 									aria-label="Close Modal"
